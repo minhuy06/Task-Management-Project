6f86108a7ab52b6db56878b21b8264dade3285ed
minhuy06 <1410huyvo@gmail.com>
2026-08-25 09:01:28 +0700

    KhInit Spring Boot project

---
+++ src/main/java/com/example/task_management_project/TaskManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementProjectApplication.class, args);
+    }
+
+}
