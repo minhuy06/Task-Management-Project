edd9792d0c8a3f3356c53a273003de478861344b
minhuy06 <1410huyvo@gmail.com>
2026-09-04 19:25:34 +0700

    add CustomerUserDetailsService.java

---
+++ backend/src/main/java/com/example/task_management_project/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.task_management_project.security;
+
+import com.example.task_management_project.entity.User;
+import com.example.task_management_project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User name not found: "+username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
