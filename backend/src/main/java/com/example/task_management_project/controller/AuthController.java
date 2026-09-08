972f7ec90d4227249b989fa1b3b8174b586ff00a
minhuy06 <1410huyvo@gmail.com>
2026-09-08 23:10:12 +0700

    add method registerUser in AuthController

--- backend/src/main/java/com/example/task_management_project/controller/AuthController.java
+++ backend/src/main/java/com/example/task_management_project/controller/AuthController.java
@@ -2,7 +2,11 @@ package com.example.task_management_project.controller;
 
 import com.example.task_management_project.dto.JwtResponseDTO;
 import com.example.task_management_project.dto.LoginRequestDTO;
+import com.example.task_management_project.dto.UserRequestDTO;
+import com.example.task_management_project.entity.User;
+import com.example.task_management_project.repository.UserRepository;
 import com.example.task_management_project.security.JwtUtils;
+import com.example.task_management_project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,11 +25,13 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final UserRepository userRepository;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, JwtUtils jwtUtils){
+    public AuthController(AuthenticationManager authenticationManager, JwtUtils jwtUtils, UserRepository userRepositor){
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
+        this.userRepository = userRepositor;
     }
 
     @PostMapping("/login")
@@ -39,5 +45,25 @@ public class AuthController {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
         return ResponseEntity.ok(new JwtResponseDTO(jwt, userDetails.getUsername()));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRequestDTO requestDTO) {
+
+        // check duplicate username
+        if (userRepository.existsByUsername(requestDTO.getUsername())) {
+            return ResponseEntity.badRequest().body("Lỗi: Tên đăng nhập đã tồn tại!");
+        }
+
+        // 2. Tạo User mới từ DTO của bạn
+        User user = new User();
+        user.setUsername(requestDTO.getUsername());
+        user.setEmail(requestDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(requestDTO.getPassword()));
+
+        // 3. Lưu xuống DB
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Đăng ký tài khoản thành công!");
     }
 }
