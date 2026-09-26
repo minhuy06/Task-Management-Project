20fc941def1ef1baa49a33c63da411b3e635eae7
minhuy06 <1410huyvo@gmail.com>
2026-09-26 21:34:58 +0700

    feat(frontend): add axiosClient.js

--- frontend/src/services/axiosClient.js
+++ frontend/src/services/axiosClient.js
@@ -10,5 +10,27 @@ const axiosClient = axios.create({
 })
 
 axios.interceptors.request.use(
-
+    (config) => {
+        const token = localStorage.getItem('jwt_token')
+        if(token){
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
 )
+
+axios.interceptors.response.use(
+    (response) => {
+        response.data
+    },
+    (error) => {
+        if(error.response && error.response.status === 401){
+            console.error("Error 401: Invalid or expired token")
+            localStorage.removeItem('jwt_token')
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
+export default axiosClient
